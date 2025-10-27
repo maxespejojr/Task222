7efb8b57752f9a8c22c2a89bdd6d9cd6971c1dca
maxespejojr <maximoespejojr@gmail.com>
2025-10-27 09:34:46 +0000

    Task2

--- Task2.java
+++ Task2.java
@@ -1,1 +1,22 @@
+public class Task2 {
+    public static void main(String[] args) {
+            // Define all primitive types (except long and double)
+        byte b = 0;
+        short s = 3110;
+        int i = 2;
+        float f = 2.0f;
+        char H = 'H';
+        char w = 'w';
+        char r = 'r';
+        char l = 'l';
+        char d = 'd';
+        boolean t = true;
 
+            // Concatenate them into a string
+       
+        String output = "" + H + s + " w" + b + r + l + d + " " + f + " " + t;
+    
+            // Printout of the String 
+            System.out.println(output);
+    }
+}
